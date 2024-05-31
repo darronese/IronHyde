922f3c1dde72e2436e2e829d9fae9b522914812f
darronese <darron198@hotmail.com>
2024-05-31 10:02:16 -0700

    Initial Commit

---
+++ IronHyde/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IronHyde
+//
+//  Created by Darron Li on 5/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IronHyde/IronHydeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IronHydeApp.swift
+//  IronHyde
+//
+//  Created by Darron Li on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct IronHydeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
