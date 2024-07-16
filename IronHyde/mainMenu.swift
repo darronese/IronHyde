82633627da63973e0d6c6152f122f015f6589869
darronese <darron198@hotmail.com>
2024-07-15 20:23:07 -0700

    created new workout plan class

---
+++ IronHyde/mainMenu.swift
@@ -0,0 +1,8 @@
+//
+//  mainMenu.swift
+//  IronHyde
+//
+//  Created by Darron Li on 6/28/24.
+//
+
+import Foundation
---
+++ IronHyde/newWorkout.swift
@@ -0,0 +1,8 @@
+//
+//  newWorkout.swift
+//  IronHyde
+//
+//  Created by Darron Li on 7/1/24.
+//
+
+import Foundation
